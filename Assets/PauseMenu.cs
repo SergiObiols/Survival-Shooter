f2cda77be5b434b16e12f5c6946297afa3519e78
Sergi Obiols Lleixà <sobiolslleixa@gmail.com>
2020-01-13 19:01:58 +0100

    PauseMenu
    Added Pause Menu Pressing/Depressing Esc during game

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject PauseMenuUI;
+    [SerializeField] public bool GamePaused;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            GamePaused = !GamePaused;
+        }
+
+        if (GamePaused)
+        {
+            ActivateMenu();
+        }
+        else
+        {
+            DeactivateMenu();
+        }
+    }
+
+    void ActivateMenu()
+    {
+        Time.timeScale = 0;
+        PauseMenuUI.SetActive(true);
+    }
+
+    public void DeactivateMenu()
+    {
+        GamePaused = false;
+        Time.timeScale = 1;
+        PauseMenuUI.SetActive(false);
+    }
+
+}
